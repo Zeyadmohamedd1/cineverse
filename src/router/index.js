60e4e43628ad1261172e12dc313aab236a259ad4
Zeyadmohamedd1 <zzyad1065@gmail.com>
2026-08-25 12:15:52 +0200

    Fix router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MoviesView from '../views/MoviesView.vue'
 import TVShowsView from '@/views/TVShowsView.vue'
